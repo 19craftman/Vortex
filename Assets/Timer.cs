a1ed8753beca2ed18fcc41ee83a18e4437ee5b98
19craftman <47764907+19craftman@users.noreply.github.com>
2020-09-02 01:56:54 -0400

    Initial commit

---
+++ Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public static event Action pull = delegate { };
+    public float time  = 10;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            pull();
+            time = 10;
+        }
+    }
+}
---
+++ Assets/Vortex.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vortex : MonoBehaviour
+{
+    public float speed = 3;
+    public GameObject player;
+    public bool pulling = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer.pull += VortexPull;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (pulling)
+        {
+            player.transform.position = Vector3.MoveTowards(player.transform.position, transform.position, step);
+        }
+        else
+        {
+            player.transform.position = player.transform.position;
+        }
+    }
+    void VortexPull()
+    {
+        StartCoroutine(Pull());
+    }
+    IEnumerator Pull()
+    {
+        pulling = false;
+        yield return new WaitForSeconds(5);
+        pulling = true;
+
+    }
+}
